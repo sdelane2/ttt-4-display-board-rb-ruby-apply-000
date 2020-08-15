c1ec85b273cfa61ad8e6f1150e64762b4f5120a4
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-15 18:04:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,5 +8,4 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-def display_board(board)
-end
+display_board(board)
